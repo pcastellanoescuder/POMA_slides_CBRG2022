7a2aa9e259ecaeda78983a43aeab2f9207bc8b34
Pol Castellano Escuder <polcaes@gmail.com>
2022-02-06 14:40:56 -0500

    preproc and EDA done

---
+++ libs/typed.js
@@ -0,0 +1,21 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    'THANK YOU VERY MUCH!', 
+    'MOLTES GRÀCIES!', 
+    'MUCHAS GRACIAS!', 
+    'GRAZIE MILLE!', 
+    'DIOLCH YN FAWR IAWN!', 
+    'شكرا جزيلا لك'
+    ],
+  typeSpeed: 50,
+  backSpeed: 20,
+  cursorChar: '|',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
